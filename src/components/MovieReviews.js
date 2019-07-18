929975df94ff39d317b5ded33c59e4b80e24cf8c
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 08:44:35 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+const Review = ({ title, img_url }) => (
+  <div className="review">
+    <img src={ img_url } />
+    <h3>{ title }</h3>
+  </div>
+)
+ 
+ 
+export default const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    { reviews.map(review => <Review title={book.title} img_url={book.image_url} />) }
+  </div>
+)
