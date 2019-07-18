b8c0db1c7d126c5475ca850a61bec5ca620e66ca
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-18 07:59:01 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'es6-promise';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
@@ -7,3 +8,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super()
+ 
+    this.state = {
+      reviews: []
+    };
+  }
+ 
+  componentDidMount() {
+    fetch(URL)
+      	.then(function(response) {
+      		if (response.status >= 400) {
+      			throw new Error("Bad response from server");
+      		}
+      		return response.json();
+      	})
+      	.then(function(stories) {
+      		console.log(stories);
+      	});
+ 
+  render() {
+    return <BookList books={this.state.books} />
+  }
+}
